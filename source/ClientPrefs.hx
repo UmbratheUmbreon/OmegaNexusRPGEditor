e97c80e4e3dcd425e4ac3732b6c58ae4ff99ff6b
AT <101066547+UmbratheUmbreon@users.noreply.github.com>
2022-07-01 15:11:03 -0700

    the bare minimum

---
+++ source/ClientPrefs.hx
@@ -0,0 +1,66 @@
+package;
+
+import flixel.FlxG;
+import flixel.util.FlxSave;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+
+class ClientPrefs {
+	public static var showFPS:Bool = true;
+	public static var globalAntialiasing:Bool = true;
+	public static var framerate:Int = 60;
+
+	public static function saveSettings() {
+		FlxG.save.data.showFPS = showFPS;
+		FlxG.save.data.globalAntialiasing = globalAntialiasing;
+		FlxG.save.data.framerate = framerate;
+		FlxG.save.flush();
+	}
+
+	public static function loadPrefs() {
+		if(FlxG.save.data.showFPS != null) {
+			showFPS = FlxG.save.data.showFPS;
+			if(Main.fpsVar != null) {
+				Main.fpsVar.visible = showFPS;
+			}
+		}
+		if(FlxG.save.data.globalAntialiasing != null) {
+			globalAntialiasing = FlxG.save.data.globalAntialiasing;
+		}
+		if(FlxG.save.data.framerate != null) {
+			framerate = FlxG.save.data.framerate;
+			if(framerate > FlxG.drawFramerate) {
+				FlxG.updateFramerate = framerate;
+				FlxG.drawFramerate = framerate;
+			} else {
+				FlxG.drawFramerate = framerate;
+				FlxG.updateFramerate = framerate;
+			}
+		}
+		// flixel automatically saves your volume!
+		if(FlxG.save.data.volume != null)
+		{
+			FlxG.sound.volume = FlxG.save.data.volume;
+		}
+		if (FlxG.save.data.mute != null)
+		{
+			FlxG.sound.muted = FlxG.save.data.mute;
+		}
+	}
+
+	public static function copyKey(arrayToCopy:Array<FlxKey>):Array<FlxKey> {
+		var copiedArray:Array<FlxKey> = arrayToCopy.copy();
+		var i:Int = 0;
+		var len:Int = copiedArray.length;
+
+		while (i < len) {
+			if(copiedArray[i] == NONE) {
+				copiedArray.remove(NONE);
+				--i;
+			}
+			i++;
+			len = copiedArray.length;
+		}
+		return copiedArray;
+	}
+}
