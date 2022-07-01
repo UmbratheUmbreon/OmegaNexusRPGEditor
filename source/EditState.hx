91a6eb8ebeb78c8fbad5a61989829bb23e0af36e
AT <101066547+UmbratheUmbreon@users.noreply.github.com>
2022-07-01 16:30:49 -0700

    path finder and moosic

--- source/EditState.hx
+++ source/EditState.hx
@@ -7,6 +7,12 @@ class EditState extends FlxState
 	override public function create()
 	{
 		super.create();
+
+		if(FlxG.sound.music == null) {
+			FlxG.sound.playMusic(Paths.music('menuMusic'), 0);
+
+			FlxG.sound.music.fadeIn(4, 0, 0.7);
+		}
 	}
 
 	override public function update(elapsed:Float)
